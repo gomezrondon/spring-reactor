7952bbc4109a2e146de0e1aad9ba12a103dbaa12
gomezrondon <gomezrondon@gmail.com>
2020-09-05 07:10:26 -0600

    1rst Commit.

---
+++ src/test/java/com/gomezrondon/springreactor/ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.gomezrondon.springreactor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+
+@Slf4j
+class ApplicationTests {
+
+	@Test
+	void test() {
+		log.info("this is a test");
+	}
+
+}
